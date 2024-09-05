ad9eba348992dfa1e502acd41e66b994391ef4d2
Adam Černý <147615015+Tucnuc@users.noreply.github.com>
2024-09-05 21:51:09 +0200

    Update 1.0

--- main.py
+++ main.py
@@ -22,9 +22,6 @@ class Visitor:
     print(f"Borrowed Books: {self.books}")
     print(f"Password: {self.password}")
 
-  def displayProfileName(self):
-    return self.name
-
 visitorDatabase = []
 takenIds = []
 loggedIn = False
@@ -39,7 +36,7 @@ def loginVisitor():
   print("")
   for visitor in visitorDatabase:
     if visitor.name == name and visitor.password == password:
-      print("Login successful. Moving to the hub..")
+      print("Login successful. Moving to the center..")
       print("")
       currentVisitor = visitor
       loggedIn = True
@@ -62,7 +59,7 @@ def loginVisitor():
 
   choice()
 
-    
+
 def registerVisitor():
   user = Visitor(generateId(), createName(), [], createPassword(), False)
   visitorDatabase.append(user)
@@ -76,8 +73,9 @@ def createName():
     if visitor.name == name:
       print("Name already taken.")
       print("")
-      createName()
+      return createName()
   return name
+
 def createPassword():
   password = input("Enter your password: ")
   password2 = input("Confirm your password: ")
@@ -87,11 +85,12 @@ def createPassword():
   else:
     print("Passwords do not match. Please try again.")
     print("")
-    createPassword()
+    return createPassword()
+    
 def generateId():
   id = random.randint(1, 10000)
   if id in takenIds:
-    generateId()
+    return generateId()
   else:
     takenIds.append(id)
     return id
@@ -125,7 +124,7 @@ def createBook():
   book = Book(name, author, isbn, status)
   bookDatabase.append(book)
   print("")
-  print("Book added successfully. Returning to The Hub..")
+  print("Book added successfully. Returning to the center..")
   print("")
   theHub()
 
@@ -137,15 +136,15 @@ def removeBook():
     print("")
     if decision == "1":
       bookDatabase.remove(selectedBookForRemoval)
-      print("Book removed successfully. Returning to The Hub..")
+      print("Book removed successfully. Returning to the center..")
       print("")
       theHub()
     elif decision == "2":
-      print("Returning to The Hub..")
+      print("Returning to the center..")
       print("")
       theHub()
     else:
-      print("Invalid choice. Try again.")
+      print("Invalid choice. Please, try again.")
       print("")
       choice()
       
@@ -163,11 +162,114 @@ def removeBook():
   removeBook()
 
 def theHub():
-  print("Welcome to The Hub. Choose where you want to go from here.")
-  return
+  print("Welcome to the center of the library. Choose where you want to go from here.")
+  print("")
+  print("1. Look into the book list.")
+  print("2. Find more information about a specific book.")
+  print("3. Borrow a book.")
+  print("4. Display account information.")
+  print("5. Leave the library.")
+  print("")
+  if currentVisitor.admin:
+    print("Administrator-exclusive options:")
+    print("6. Add a book into the library.")
+    print("7. Remove a book from the library.")
+    print("8. Check a list of registered accounts.")
+    print("9. Find more information about a specific account.")
+    print("10. Delete an account from the database.")
+    print("")
+  
+  def choice():
+    global currentVisitor
+
+    def pause():
+      print("")
+      input("Press Enter to continue: ")
+      print("")
+      theHub()
+
+    decision = input("Choose an action. [number]: ")
+    print("")
+    if decision == "1":
+      print("Here's the list of all books:")
+      index = 1
+      for book in bookDatabase:
+        print(f"{index}. {book.name}")
+        index += 1
+      pause()
+      return
+    elif decision == "2":
+      print("Info here..")
+      pause()
+      return
+    elif decision == "3":
+      print("Borrowing a book..")
+      pause()
+      return
+    elif decision == "4":
+      print("Here's your detailed account information:")
+      currentVisitor.displayProfile()
+      pause()
+      return
+    elif decision == "5":
+      print(f"We wish you a safe journey, {currentVisitor.name}.")
+      print("")
+      currentVisitor = ""
+      startingPoint()
+      return
+    elif decision == "6" and currentVisitor.admin:
+      print("Starting the book adding procedure..")
+      createBook()
+      return
+    elif decision == "7" and currentVisitor.admin:
+      print("Starting the book removal procedure..")
+      removeBook()
+      return
+    elif decision == "8" and currentVisitor.admin:
+      print("Displaying the list of registered accounts:")
+      for visitor in visitorDatabase:
+        print(f"ID: {visitor.id} | Name: {visitor.name}")
+      pause()
+      return
+    elif decision == "9" and currentVisitor.admin:
+      print("Displaying..")
+      pause()
+      return
+    elif decision == "10" and currentVisitor.admin:
+      print("Deleting..")
+      pause()
+      return
+    else:
+      print("Invalid choice. Please, try again.")
+      print("")
+      choice()
+    
+  choice()
 
 
+def startingPoint():
+  print("Welcome, visitor. Before you can access all the knowledge on the continent, you need to verify yourself.")
+  
+  def choice():
+    decision = input("Either login into an existing account or register a new one. [1 - Login, 2 - Registration]: ")
+    print("")
+    if decision == "1":
+      print("Moving to the Login Page..")
+      print("")
+      loginVisitor()
+    elif decision == "2":
+      print("Moving to the Registration Page..")
+      print("")
+      registerVisitor()
+    else:
+      print("Invalid choice. Please, try again.")
+      print("")
+      choice()
 
-print("Welcome to the Royal Library. Please login or register.\n")
-loginVisitor()
-print(currentVisitor.displayProfile())
+  choice()
+
+administrator = Visitor("1", "Adam", [], "1235", True)
+visitorDatabase.append(administrator)
+takenIds.append(administrator.id)
+
+startingPoint()
